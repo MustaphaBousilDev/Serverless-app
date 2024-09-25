f96b7471eae35f38da8da0a07c9b4c8bc60b3ceb
Mustapha bousil <bousilmustapha@gmail.com>
2024-09-25 11:17:09 +0100

    finish crud operation

--- handler.js
+++ handler.js
@@ -2,7 +2,23 @@
 
 module.exports.createProduct = async (event) => {
   return {
-    statusCode: 200,
+    statusCode: 201,
     body: JSON.stringify('A new product created'),
   };
 };
+//update product
+module.exports.updateProduct = async (event) => {
+  let productId = 6
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`The not with  : ${productId}`),
+  };
+};
+
+module.exports.deleteProduct = async (event) => {
+  let nodeList = 6;
+  return {
+    statusCode: 200,
+    body: JSON.stringify("The not with id" + nodeList + "has ben deleted")
+  }
+}
