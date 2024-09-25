a5f77530861b1fa052cb5247ba0e57439566a430
Mustapha bousil <bousilmustapha@gmail.com>
2024-09-25 14:07:48 +0100

    integrate SDK AWS and insert into DynamoDB

--- handler.js
+++ handler.js
@@ -1,6 +1,33 @@
 'use strict';
+const DynamoDB = require('aws-sdk/clients/dynamodb')
+const documentClient = new DynamoDB.DocumentClient({ region: 'us-east-1' })
 
-module.exports.createProduct = async (event) => {
+
+
+module.exports.createProduct = async (event, context, cb) => {
+  let data = JSON.parse(event.body)
+  try {
+    const params = {
+      TableName: "products",
+      Item: {
+        productsId: data.id,
+        title: data.title,
+        body: data.body
+      },
+      ConditionExpression: 'attribute_not_exists(productsId)'
+    }
+    await documentClient.put(params).promise()
+    cb(null,{
+      statusCode: 200,
+      body: JSON.stringify(data),
+    })
+  } catch(err) {
+    console.log(err)
+    cb(null,{
+      statusCode: 500,
+      body: JSON.stringify(err.message),
+    })
+  }
   return {
     statusCode: 201,
     body: JSON.stringify('A new product created'),
