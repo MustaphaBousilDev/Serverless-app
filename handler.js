dd0106f54b1d7dc7efb220c4d3e5184721dc4f9e
Mustapha bousil <bousilmustapha@gmail.com>
2024-09-26 12:28:56 +0100

    finish updating operation lambda function

--- handler.js
+++ handler.js
@@ -1,46 +1,62 @@
 'use strict';
 const DynamoDB = require('aws-sdk/clients/dynamodb')
 const documentClient = new DynamoDB.DocumentClient({ region: 'us-east-1' })
+const PRODUCTS_TABLE_NAME = process.env.PRODUCTS_TABLE_NAME
 
-
+const send = (statusCode, data) =>{
+  return {
+    statusCode,
+    body: JSON.stringify(data)
+  }
+}
 
 module.exports.createProduct = async (event, context, cb) => {
   let data = JSON.parse(event.body)
   try {
     const params = {
-      TableName: "products",
+      TableName: PRODUCTS_TABLE_NAME,
       Item: {
         productsId: data.id,
         title: data.title,
-        body: data.body
+        body: data.body,
+        to: 'rttrt'
       },
       ConditionExpression: 'attribute_not_exists(productsId)'
     }
     await documentClient.put(params).promise()
-    cb(null,{
-      statusCode: 200,
-      body: JSON.stringify(data),
-    })
+    cb(null,send(201,data))
   } catch(err) {
     console.log(err)
-    cb(null,{
-      statusCode: 500,
-      body: JSON.stringify(err.message),
-    })
+    cb(null,send(500,err.message))
   }
-  return {
-    statusCode: 201,
-    body: JSON.stringify('A new product created'),
-  };
 };
 //update product
-module.exports.updateProduct = async (event) => {
-  let productId = 6
-  return {
-    statusCode: 200,
-    body: JSON.stringify(`The not with  : ${productId}`),
-  };
-};
+//update product
+module.exports.updateProduct = async (event, context, cb) => {
+  let productsId = event.pathParameters.id
+  let data = JSON.parse(event.body) // Fixing JSON.parse typo
+  try {
+    const params = {
+      TableName: PRODUCTS_TABLE_NAME,
+      Key: { productsId }, // Fixing 'Key' casing
+      UpdateExpression: 'set #title = :title, #body = :body',
+      ExpressionAttributeNames: {
+        '#title': 'title',
+        '#body': 'body'
+      },
+      ExpressionAttributeValues: {
+        ':title': data.title,
+        ':body': data.body
+      },
+      ConditionExpression: 'attribute_exists(productsId)' // Ensure the product exists before updating
+    }
+    await documentClient.update(params).promise()
+    cb(null, send(200, data)) // Respond with success
+  } catch (error) {
+    console.log(error)
+    cb(null, send(500, error.message)) // Handle error
+  }
+}
 
 module.exports.deleteProduct = async (event) => {
   let nodeList = 6;
